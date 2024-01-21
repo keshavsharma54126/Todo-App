052d8c316f0d66623a3c409659853d9cc082ca69
root <root@DESKTOP-HCNR8I1>
2024-01-22 01:41:55 +0530

    added input validation

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 //write basic express boilerplate code.
+//validate inputs with zod
 
 const express = require('express');
+const {createTodo} = require('./types');
+const {updateTodo} = require('./types');
 
 const app = express();
+app.use(express.json());
 
-app.get('/',(req,res)=>{
+
+
+app.post('/todo',(req,res)=>{
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        res.json({
+            msg:"you send the wrong details"
+        })
+        return;
+    }
+    //put it in mongodb
+
+    
+})
+
+
+app.get('/todo',(req,res)=>{
     res.send("haa haa")
+})
+
+app.put('/completed',(req,res)=>{
+    const updatePayload = req.body;
+    const parsePayload = updatePayload.safeParse(updatePayload);
+    if(!parsePayload.success){
+        res.json({
+            msg:"you send the wrong details"
+        })
+        return;
+    }
+    
 })
 
 app.listen(3000,()=>{
