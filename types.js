b5992a80d28be22be0a85e6eba46f57104607df7
root <root@DESKTOP-HCNR8I1>
2024-01-22 02:18:58 +0530

    minor bug fix

--- types.js
+++ types.js
@@ -1,14 +1,14 @@
 const zod = require("zod")
 
 const createTodo = zod.object({
-    title: zod.string(),
-    descryption: zod.string()
+    title: zod.string().min(1),
+    descryption: zod.string().min(1)
 })
 const updateTodo = zod.object({
     id:zod.string()
 })
 
-module.export={
+module.exports={
     createTodo: createTodo,
     updateTodo: updateTodo
 }
